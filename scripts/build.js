000d97359204eafe9d71cdffb4c70ec3f9bff9be
Josh Kolenko <joshua.kolenko_ic@wwwinc.com>
2022-06-30 13:24:25 -0400

    Add time format

--- scripts/build.js
+++ scripts/build.js
@@ -18,9 +18,23 @@ const build = async () => {
   const seconds = startTime.getSeconds()
   const ampm = hours >= 12 ? 'PM' : 'AM'
 
+  const formatTime = (h, m, s) => {
+    const leadingZero = num => {
+      if (num < 10) return '0' + num
+      return num
+    }
+
+    h = h % 12
+    h = leadingZero(h)
+    m = leadingZero(m)
+    s = leadingZero(s)
+
+    return ` ${h}:${m}:${s} ${ampm} `
+  }
+
   console.log(
-    chalk.bgWhiteBright(` ${hours % 12}:${minutes}:${seconds} ${ampm} `) +
-      ` Starting build...`
+    chalk.bgWhiteBright(formatTime(hours, minutes, seconds)) +
+      ` Starting build...\n`
   )
 
   const configPath = path.join(INIT_CWD, 'config.json')

5e9d2862fb11dedcfc60a0b2d55dd0a69046b28d
Josh Kolenko <95929281+joshkolenko@users.noreply.github.com>
2023-02-24 09:59:00 -0500

    feat: Change created config file from json to js with types

--- lib/init/config.ts
+++ lib/init/config.ts
@@ -1,62 +1,79 @@
 import prompts from 'prompts';
 import path from 'path';
+import prettier from 'prettier';
 import fs from 'fs';
 import chalk from 'chalk';
 import figures from 'figures';
 
 import type { ConfigObj } from '../types/config.js';
 
-export const configPath = path.resolve(process.cwd(), 'config.json');
+export const configPath = path.resolve(process.cwd(), 'config.js');
 
 export async function generateConfig() {
-  if (fs.existsSync(configPath)) {
-    const response = await prompts({
-      type: 'confirm',
-      name: 'continue',
-      message: `Generating a new ${chalk.yellow(
-        'config.json'
-      )} will override the old one. Continue?`,
-    });
+  const defaults = {
+    attribute: 'inline',
+    outputDir: 'dist',
+  };
 
-    if (!response.continue) process.exit();
-  }
+  try {
+    if (fs.existsSync(configPath)) {
+      const response = await prompts({
+        type: 'confirm',
+        name: 'continue',
+        message: `Generating a new ${chalk.yellow(
+          'config.js'
+        )} will override the existing one. Continue?`,
+      });
 
-  const responses: ConfigObj = await prompts(
-    [
-      {
-        type: 'text',
-        name: 'developer',
-        message: 'What are your initials?',
-      },
-      {
-        type: 'text',
-        name: 'attribute',
-        message: 'Attribute for bundling',
-        initial: 'inline',
-      },
-      {
-        type: 'text',
-        name: 'stylesDir',
-        message: 'Styles directory',
-        initial: 'styles',
-      },
-      {
-        type: 'text',
-        name: 'jsDir',
-        message: 'JavaScript directory',
-        initial: 'js',
-      },
-    ],
-    {
-      onCancel: () => process.exit(),
+      if (!response.continue) process.exit();
     }
-  );
 
-  fs.writeFileSync(configPath, JSON.stringify(responses as ConfigObj));
+    const responses = await prompts(
+      [
+        {
+          type: 'text',
+          name: 'developer',
+          message: 'What are your initials?',
+        },
+        {
+          type: 'text',
+          name: 'stylesDir',
+          message: 'Styles directory',
+          initial: 'styles',
+        },
+        {
+          type: 'text',
+          name: 'jsDir',
+          message: 'JavaScript directory',
+          initial: 'js',
+        },
+      ],
+      {
+        onCancel: () => process.exit(),
+      }
+    );
 
-  console.log(
-    `${chalk.green(figures.tick)} ${chalk.yellow(
-      'config.json'
-    )} created at: ${chalk.dim(configPath)}`
-  );
+    const config: ConfigObj = { ...defaults, ...responses };
+
+    fs.writeFileSync(
+      configPath,
+      prettier.format(
+        `/** @type {import("./lib/types/config.js").ConfigObj} */\n\n` +
+          `export const config = ${JSON.stringify(config)}`,
+        {
+          parser: 'babel',
+        }
+      )
+    );
+
+    console.log(
+      `${chalk.green(figures.tick)} ${chalk.yellow(
+        'config.js'
+      )} created at: ${chalk.dim(configPath)}`
+    );
+  } catch (error) {
+    console.error(error);
+  } finally {
+    process.exit();
+  }
 }

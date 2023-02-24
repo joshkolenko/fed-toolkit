8d619a0b1f3ece66bcc2aeaec4d43de398161bc7
Josh Kolenko <95929281+joshkolenko@users.noreply.github.com>
2023-02-24 09:34:30 -0500

    chore: Add more config properties

--- lib/types/config.d.ts
+++ lib/types/config.d.ts
@@ -1,6 +1,19 @@
 export interface ConfigObj {
+  [key: string]: any;
+  /** Developer initials - Example: JMK */
   developer: string;
+  /** Attribute used for inlining html tags. Defaults to `inline` */
   attribute: string;
+  /** Output directory for inlined html files. Defaults to `dist` */
+  outputDir: string;
+  /** Option to clean contents of `outputDir` on build. Defaults to `true` */
+  cleanOutputDir?: boolean;
+  /** Directory for css & scss files. Defaults to `styles` */
   stylesDir: string;
+  /** Directory for js files. Defaults to `js` */
   jsDir: string;
+  /** Array of paths to be ignored by watcher. */
+  ignoredPaths?: string[];
+  /** Option to copy ticket path to clipboard after scaffolding a new ticket */
+  copyTicketPath?: boolean;
 }
